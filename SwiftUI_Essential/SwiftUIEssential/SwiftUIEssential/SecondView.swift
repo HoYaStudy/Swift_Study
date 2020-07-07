c72b68cdc5f9a456e85af65725282340539e8b51
llHoYall <hoya128@gmail.com>
2020-07-07 20:36:25 +0900

    Added SwiftUI_Essential (book)

---
+++ SwiftUI_Essential/SwiftUIEssential/SwiftUIEssential/SecondView.swift
@@ -0,0 +1,29 @@
+//
+//  SecondView.swift
+//  SwiftUIEssential
+//
+//  Created by hoya kim on 2020/06/15.
+//  Copyright © 2020 hoya kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    @EnvironmentObject var timerData: TimerData
+    
+    var body: some View {
+        VStack {
+            Text("Second View")
+                .font(.largeTitle)
+            Text("Timer Count = \(timerData.timeCount)")
+                .font(.headline)
+        }
+        .padding()
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView().environmentObject(TimerData())
+    }
+}
